e79083d86f94d7a153beab79d350d8b500628289
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-10 10:03:54 +0200

    Add routes

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-var express = require("express");
-var app = express();
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const users = require("./src/api/users.js");
+const photos = require("./src/api/photos.js");
+const session = require("express-session");
+const { Strategy: LocalStrategy } = require("passport-local");
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+app.use(
+  session({
+    secret: process.env.APP_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/auth", login);
+app.use("/api/users", users);
+app.use("/api/photos", photos);
+
+// Handle requests for SPA
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 const server = app.listen(port, async () => {
   console.log(`Listening on port ${server.address().port}`);
