442cc65a3fd0effcbe29f0dd6d1690eb9f9dd340
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-10 09:53:00 +0200

    Fetch feed from API

--- frontend/src/screens/Feed.js
+++ frontend/src/screens/Feed.js
@@ -5,23 +5,43 @@ import Footer from "../Card/Footer";
 import Comments from "../Card/Comments";
 import Comment from "../Card/Comment";
 import SiteContainer from "../UI/SiteContainer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const FeedCard = ({ body, path }) => {
+  return (
+    <Card>
+      <Heading name="cat_person" href="#nowhere" />
+      <Image alt={body} image={path} />
+
+      <Footer likes={300} name={"cat_person"} body={body}>
+        <Comments>
+          <Comment name="asd" body="testing" />
+        </Comments>
+      </Footer>
+    </Card>
+  );
+};
 
 const Feed = () => {
-  return (
-    <SiteContainer className="flex items-center gap-2">
-      <Card>
-        <Heading name="cat_person" href="#nowhere" />
-        <Image
-          alt="This is my cat steve"
-          image="https://placekitten.com/1080/1080"
-        />
+  const [photos, setPhotos] = useState([]);
 
-        <Footer likes={300} name={"cat_person"} body="This is my cat steve">
-          <Comments>
-            <Comment>cute cat</Comment>
-          </Comments>
-        </Footer>
-      </Card>
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        process.env.REACT_APP_BACKEND_ENDPOINT + "/api/photos"
+      );
+
+      console.log(response.data);
+      setPhotos(response.data);
+    })();
+  }, []);
+
+  return (
+    <SiteContainer className="flex flex-wrap items-center gap-2">
+      {photos.map((photo) => (
+        <FeedCard key={photo.id} body={photo.body} path={photo.path} />
+      ))}
     </SiteContainer>
   );
 };
