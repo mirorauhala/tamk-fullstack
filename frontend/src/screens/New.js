bfdfbb1821ebd39e8d287c5b8efa9a6fba0f1b3e
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-10 21:09:40 +0200

    Add rudimentary image upload and cropping

--- frontend/src/screens/New.js
+++ frontend/src/screens/New.js
@@ -1,7 +1,8 @@
 import SiteContainer from "../UI/SiteContainer";
 import Cropper from "react-easy-crop";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import getCroppedImg from "../cropImage";
+import axios from "axios";
 
 function readFile(file) {
   return new Promise((resolve) => {
@@ -13,24 +14,14 @@ function readFile(file) {
 
 const New = () => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const bodyRef = useRef();
   const [zoom, setZoom] = useState(1);
   const [imageSrc, setImageSrc] = useState(null);
-  const [croppedImage, setCroppedImage] = useState(null);
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
 
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
     setCroppedAreaPixels(croppedAreaPixels);
   }, []);
-
-  const showCroppedImage = useCallback(async () => {
-    try {
-      const croppedImage = await getCroppedImg(imageSrc, croppedAreaPixels);
-      console.log("donee", { croppedImage });
-      setCroppedImage(croppedImage);
-    } catch (e) {
-      console.error(e);
-    }
-  }, [croppedAreaPixels]);
 
   const onFileChange = async (e) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -41,12 +32,76 @@ const New = () => {
     }
   };
 
+  const getImage = async () => {
+    try {
+      return await getCroppedImg(imageSrc, croppedAreaPixels);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  /**
+   * Handle publishing new image.
+   *
+   * @returns {Promise<void>}
+   */
+  const handlePublish = async () => {
+    const image = await getImage();
+
+    // fetch pre-signed upload
+    const presignedResponse = await axios.get(
+      process.env.REACT_APP_BACKEND_ENDPOINT + "/api/photos/presigned-url",
+      { type: image.type }
+    );
+
+    const formData = new FormData();
+    formData.append("key", presignedResponse.data.fields["Key"]);
+    formData.append("acl", presignedResponse.data.fields["acl"]);
+    formData.append("bucket", presignedResponse.data.fields["bucket"]);
+    formData.append("X-Amz-Algorithm", presignedResponse.data.fields["X-Amz-Algorithm"]); // prettier-ignore
+    formData.append("X-Amz-Credential", presignedResponse.data.fields["X-Amz-Credential"]); // prettier-ignore
+    formData.append("X-Amz-Date", presignedResponse.data.fields["X-Amz-Date"]);
+    formData.append("Policy", presignedResponse.data.fields["Policy"]);
+    formData.append("X-Amz-Signature", presignedResponse.data.fields["X-Amz-Signature"]); // prettier-ignore
+    formData.append("file", await getBlob(image));
+
+    // upload image
+    const uploadResponse = await axios.post(
+      presignedResponse.data.url,
+      formData
+    );
+
+    const publishResponse = await axios.post(
+      process.env.REACT_APP_BACKEND_ENDPOINT + "/api/photos",
+      {
+        path: `${presignedResponse.data.url}/${presignedResponse.data.fields["Key"]}`,
+        body: bodyRef.current.value,
+      }
+    );
+
+    console.log(publishResponse.data);
+  };
+
+  const getBlob = async (fileUri) => {
+    const resp = await fetch(fileUri);
+    return await resp.blob();
+  };
+
   return (
     <SiteContainer className="flex flex-col items-center gap-2">
       <h1 className="text-left w-full mb-2 font-medium text-lg">New post</h1>
 
       {!imageSrc ? (
-        <input type="file" onChange={onFileChange} accept="image/*" />
+        <input
+          type="file"
+          onChange={onFileChange}
+          accept="image/*"
+          className="block w-full text-sm text-neutral-500
+            file:px-4 file:mr-3 file:py-2 file:rounded-2xl file:border-0
+            file:text-sm file:font-semibold file:bg-white
+            file:text-violet-700
+            hover:file:bg-violet-100"
+        />
       ) : (
         <>
           <div className="relative top-0 aspect-square w-full overflow-hidden">
@@ -73,7 +128,6 @@ const New = () => {
             onChange={(e) => {
               setZoom(e.target.value);
             }}
-            className="zoom-range"
           />
 
           <form className="flex flex-col gap-1">
@@ -81,6 +135,7 @@ const New = () => {
             <input
               type="text"
               id="bodyInput"
+              ref={bodyRef}
               className="border px-3 py-1 mb-3"
               name="body"
             />
@@ -88,7 +143,7 @@ const New = () => {
             <button
               type="button"
               className="bg-white text-black px-4 py-2 rounded-xl font-medium border-2"
-              onClick={showCroppedImage}
+              onClick={handlePublish}
             >
               Publish
             </button>
