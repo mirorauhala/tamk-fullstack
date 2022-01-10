02084c244bc889876ea8ea1dda6a3947d768a3f2
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-10 11:17:50 +0200

    Add active state & new post link

--- frontend/src/UI/Navbar.js
+++ frontend/src/UI/Navbar.js
@@ -1,4 +1,17 @@
-import { HeartIcon, HomeIcon, UserCircleIcon } from "@heroicons/react/outline";
+import {
+  HeartIcon,
+  HomeIcon,
+  PlusCircleIcon,
+  UserCircleIcon,
+} from "@heroicons/react/outline";
+
+import {
+  HeartIcon as HeartIconSolid,
+  HomeIcon as HomeIconSolid,
+  PlusCircleIcon as PlusCircleIconSolid,
+  UserCircleIcon as UserCircleIconSolid,
+} from "@heroicons/react/solid";
+
 import { Link, NavLink } from "react-router-dom";
 import SiteContainer from "./SiteContainer";
 
@@ -19,15 +32,55 @@ const Navbar = () => {
         </form>
         <div className="ml-auto flex gap-3">
           <NavLink to={"/"}>
-            <HomeIcon className="w-6 h-6" />
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <HomeIconSolid className="w-6 h-6" />
+                ) : (
+                  <HomeIcon className="w-6 h-6" />
+                )}
+                <span className="sr-only">Home</span>
+              </>
+            )}
+          </NavLink>
+
+          <NavLink to={"/new"}>
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <PlusCircleIconSolid className="w-6 h-6" />
+                ) : (
+                  <PlusCircleIcon className="w-6 h-6" />
+                )}
+                <span className="sr-only">New post</span>
+              </>
+            )}
           </NavLink>
 
           <NavLink to={"/liked"}>
-            <HeartIcon className="w-6 h-6" />
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <HeartIconSolid className="w-6 h-6" />
+                ) : (
+                  <HeartIcon className="w-6 h-6" />
+                )}
+                <span className="sr-only">Liked</span>
+              </>
+            )}
           </NavLink>
 
           <NavLink to={"/settings"}>
-            <UserCircleIcon className="w-6 h-6" />
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <UserCircleIconSolid className="w-6 h-6" />
+                ) : (
+                  <UserCircleIcon className="w-6 h-6" />
+                )}
+                <span className="sr-only">Settings</span>
+              </>
+            )}
           </NavLink>
         </div>
       </SiteContainer>
