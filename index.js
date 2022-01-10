c37735c1a58314df0453aedbc2de10311aad22df
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-10 09:59:52 +0200

    Close server on kill signal

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ app.use(express.static("public"));
 const server = app.listen(port, async () => {
   console.log(`Listening on port ${server.address().port}`);
 });
+
+process.on("SIGTERM", () => {
+  server.close(() => console.log("Server closed"));
+  process.exit(0);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => console.log("Server closed"));
+  process.exit(0);
+});
+
+process.on("STOP", () => {
+  server.close(() => console.log("Server closed"));
+  process.exit(0);
+});
