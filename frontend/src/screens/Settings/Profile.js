d14f8f465bb44ca39dcc61d02442ca3f7af5d690
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-12 18:45:59 +0200

    Show current user information

--- frontend/src/screens/Settings/Profile.js
+++ frontend/src/screens/Settings/Profile.js
@@ -1,38 +1,72 @@
 import InputGroup from "../../UI/Settings/InputGroup";
+import { UserContext } from "../../UserContext";
+import { useContext, useEffect } from "react";
 
 const Profile = () => {
+  const context = useContext(UserContext);
+
   return (
     <form className="flex flex-col">
       <InputGroup>
         <div className="w-1/4 pr-3">
-          <label className="block w-full text-right" htmlFor="nameInput">
-            Nimi
+          <label className="block w-full text-right" htmlFor="firstNameInput">
+            First Name
           </label>
         </div>
         <div className="w-3/4">
-          <input type="text" id="nameInput" className="border w-2/3" />
+          <input
+            type="text"
+            id="firstNameInput"
+            className="border w-2/3"
+            defaultValue={context.user.first_name}
+          />
+        </div>
+      </InputGroup>
+
+      <InputGroup>
+        <div className="w-1/4 pr-3">
+          <label className="block w-full text-right" htmlFor="lastNameInput">
+            Last Name
+          </label>
+        </div>
+        <div className="w-3/4">
+          <input
+            type="text"
+            id="lastNameInput"
+            className="border w-2/3"
+            defaultValue={context.user.last_name}
+          />
         </div>
       </InputGroup>
 
       <InputGroup>
         <div className="w-1/4 pr-3">
           <label className="block w-full text-right" htmlFor="userNameInput">
-            Käyttäjänimi
+            Username
           </label>
         </div>
         <div className="w-3/4">
-          <input type="text" id="userNameInput" className="border w-2/3" />
+          <input
+            type="text"
+            id="userNameInput"
+            className="border w-2/3"
+            defaultValue={context.user.username}
+          />
         </div>
       </InputGroup>
 
       <InputGroup>
         <div className="w-1/4 pr-3">
           <label className="block w-full text-right" htmlFor="bioInput">
-            Elämänkerta
+            Bio
           </label>
         </div>
         <div className="w-3/4">
-          <textarea id="bioInput" className="border w-2/3" />
+          <textarea
+            id="bioInput"
+            className="border w-2/3"
+            defaultValue={context.user.bio}
+          />
         </div>
       </InputGroup>
     </form>
