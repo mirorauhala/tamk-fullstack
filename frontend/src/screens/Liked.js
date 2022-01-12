d6eee341f6ee1c15ba9e200e785eee04e8fe6ca1
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-12 21:32:58 +0200

    Remove comments

--- frontend/src/screens/Liked.js
+++ frontend/src/screens/Liked.js
@@ -1,9 +1,6 @@
 import Card from "../Card/Card";
 import Heading from "../Card/Heading";
 import Image from "../Card/Image";
-import Footer from "../Card/Footer";
-import Comments from "../Card/Comments";
-import Comment from "../Card/Comment";
 import SiteContainer from "../UI/SiteContainer";
 
 const Liked = () => {
@@ -16,12 +13,6 @@ const Liked = () => {
           alt="This is my cat steve"
           image="https://placekitten.com/1080/1080"
         />
-
-        <Footer likes={300} name={"cat_person"} body="This is my cat steve">
-          <Comments>
-            <Comment>cute cat</Comment>
-          </Comments>
-        </Footer>
       </Card>
     </SiteContainer>
   );
