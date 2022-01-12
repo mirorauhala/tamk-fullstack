7a91185fccc027e7528b8920db58e6bfa631b9a0
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-01-12 18:45:16 +0200

    Add user context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,38 @@ import Settings from "./screens/Settings/Settings";
 import Profile from "./screens/Profile";
 import New from "./screens/New";
 import Photo from "./screens/Photo";
+import { UserContext } from "./UserContext";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  /**
+   * Load authenticated user from server.
+   * @returns {Promise<void>}
+   */
+  const reloadUser = async () => {
+    const requestedUser = await axios.get(
+      process.env.REACT_APP_BACKEND_ENDPOINT + "/api/me"
+    );
+
+    setUser(requestedUser.data);
+  };
+
+  // fetch user data every 10 seconds
+  useEffect(() => {
+    // load user immediately
+    reloadUser();
+    const intervalId = setInterval(reloadUser, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <>
+    <UserContext.Provider value={{ user, reloadUser }}>
       <Navbar />
       <Routes>
         <Route path="*" element={<NotFound />} />
@@ -21,7 +49,7 @@ function App() {
         <Route path="/settings/*" element={<Settings />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </>
+    </UserContext.Provider>
   );
 }
 
